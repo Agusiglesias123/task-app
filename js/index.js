86617caf0c4e09dddf34e39a46afac14840728d9
Iglesias Agustin <agustiniglesias09@hotmail.com>
2023-03-24 23:11:29 -0300

    v-0-1-1

--- js/index.js
+++ js/index.js
@@ -1,11 +1,29 @@
-let listaInput = document.getElementById('listaInput')
+// DOM
 let buttonEnviar = document.getElementById('buttonEnviar')
 let list = document.getElementById('list')
 let ul = document.getElementById('ul')
+let listaInput = document.getElementById('listaInput')
+let buttonEliminarTodo = document.getElementById('eliminarTodo')
 
 
 
-buttonEnviar.addEventListener('click',(e) =>{
+let tareas = []
+if(localStorage.getItem("tareas")){
+    tareas = JSON.parse(localStorage.getItem("tareas"))
+}else{
+    console.log("Seteando historial de productos")
+    localStorage.setItem("tareas", JSON.stringify(tareas))
+}
+
+
+mostrarTareas(tareas);
+
+
+// EVENTOS
+buttonEnviar.addEventListener('click',(e)=>{cargarTask(e, tareas)})
+
+
+function cargarTask(e, array){
     e.preventDefault();
     let text = listaInput.value
     if (text !== '' ){
@@ -14,9 +32,46 @@ buttonEnviar.addEventListener('click',(e) =>{
         p.textContent = text;
         li.appendChild(p);
         ul.appendChild(li);
-        listaInput.value = '';
-        console.log('error')
+        array.push(text)
+        localStorage.setItem("tareas", JSON.stringify(array))
+        mostrarTareas(array)
     }
-})
+    listaInput.value = '';
+}
+
+function mostrarTareas(array) {
+    ul.innerHTML = '';
+    array.forEach((tarea, index) => {
+        let li = document.createElement('li');
+        let p = document.createElement('p');
+        p.textContent = tarea;
+        li.appendChild(p);
+        let btnEliminar = document.createElement('i');
+        btnEliminar.classList = 'btnEliminar bi bi-trash3-fill';
+        btnEliminar.addEventListener('click', () => {
+            array.splice(index, 1);
+            localStorage.setItem("tareas", JSON.stringify(array));
+            mostrarTareas(array);
+        });
+        li.appendChild(btnEliminar);
+        ul.appendChild(li);
+    });
+
+    buttonEliminarTodo.addEventListener('click', () => {
+        localStorage.removeItem('tareas');
+        ul.innerHTML = '';
+        tareas = [];
+    });
+}
+
+
+
+
+
+
+
+
+
+
 
 
